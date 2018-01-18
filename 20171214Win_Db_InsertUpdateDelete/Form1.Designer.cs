b7feb49a929c2539ee1965f9699758df34a93999
Fuad Gulmammadov <fgulmemmedov1@std.qu.edu.az>
2018-01-18 13:26:14 +0400

    initial commit

---
+++ 20171214Win_Db_InsertUpdateDelete/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace _20171214Win_Db_InsertUpdateDelete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBali = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTNomresi = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(368, 281);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(493, 99);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtAdi.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(462, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Adı:";
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(493, 125);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyadi.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(445, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soyadı:";
+            // 
+            // txtBali
+            // 
+            this.txtBali.Location = new System.Drawing.Point(493, 155);
+            this.txtBali.Name = "txtBali";
+            this.txtBali.Size = new System.Drawing.Size(100, 20);
+            this.txtBali.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(460, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Balı:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(505, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Əlavə Et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Axtar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(403, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tələbə Nömrəsi:";
+            // 
+            // txtTNomresi
+            // 
+            this.txtTNomresi.Location = new System.Drawing.Point(493, 181);
+            this.txtTNomresi.Name = "txtTNomresi";
+            this.txtTNomresi.Size = new System.Drawing.Size(100, 20);
+            this.txtTNomresi.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 391);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTNomresi);
+            this.Controls.Add(this.txtBali);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBali;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTNomresi;
+    }
+}
+
---
+++ 20171214Win_Db_InsertUpdateDelete/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace _20171214Win_Db_InsertUpdateDelete
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=.\TMS.accdb;Persist Security Info=False;";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "";
+            OleDbConnection conn = new OleDbConnection(connectionString);
+            if (textBox1.Text =="")
+            {
+                sorgu = "Select * from telebe";
+            }
+            else
+            {
+                sorgu = string.Format("Select * from telebe where Adi='{0}'",textBox1.Text);
+            }
+            OleDbCommand cmd = new OleDbCommand(sorgu,conn);
+            DataTable dt = new DataTable();     
+            conn.Open();
+            dt.Load(cmd.ExecuteReader());
+            conn.Close();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sorgu = "";
+            OleDbConnection conn = new OleDbConnection(connectionString);
+
+            sorgu = string.Format("Insert into telebe (Adi, Soyadi, Bali, TelebeNomresi) values('{0}','{1}','{2}','{3}')",txtAdi.Text,txtSoyadi.Text,txtBali.Text,txtTNomresi.Text);
+            OleDbCommand cmd = new OleDbCommand(sorgu, conn);
+            DataTable dt = new DataTable();
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            button1_Click(null,null);
+        }
+
+
+    }
+}
